97885f122e514055673bdd1aac711eb901034af5
santhiya <sandhiya1606@gmail.com>
2018-06-12 12:58:49 +0530

    Initial commit

---
+++ app/src/main/java/com/nickteck/schoolapp/activity/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.nickteck.schoolapp.activity;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+import com.nickteck.schoolapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+
+        init();
+
+
+    }
+
+    private void init() {
+        videoView  = (VideoView) findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.background_video);
+
+        videoView.setDrawingCacheEnabled(true);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+    }
+}
