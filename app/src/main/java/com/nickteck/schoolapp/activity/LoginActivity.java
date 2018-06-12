3f85bae1731977b85e2da74c683de8413a6d924f
Priya <priyadharsini586@gmail.com>
2018-06-12 19:46:20 +0530

    video file

--- app/src/main/java/com/nickteck/schoolapp/activity/LoginActivity.java
+++ app/src/main/java/com/nickteck/schoolapp/activity/LoginActivity.java
@@ -4,15 +4,20 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.VideoView;
 
 import com.nickteck.schoolapp.R;
 
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
+
+
 public class LoginActivity extends AppCompatActivity {
 
     VideoView mVideoView;
+    CircularProgressButton btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +25,32 @@ public class LoginActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
         init();
 
     }
 
     private void init() {
         mVideoView  = (VideoView) findViewById(R.id.videoView);
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.wildlife);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.app_background);
         mVideoView.setDrawingCacheEnabled(true);
         mVideoView.setVideoURI(uri);
         mVideoView.requestFocus();
         mVideoView.start();
-
         mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
                 mediaPlayer.setLooping(true);
             }
         });
+
+        btnSubmit = (CircularProgressButton) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnSubmit.startAnimation();
+            }
+        });
+
     }
 }
