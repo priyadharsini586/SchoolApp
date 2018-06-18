5f6e7b0bf7e10cfa45c34ec71689bda3e0a87d47
Priya <priyadharsini586@gmail.com>
2018-06-18 17:09:10 +0530

    parent details

--- app/src/main/java/com/nickteck/schoolapp/model/ParentDetails.java
+++ app/src/main/java/com/nickteck/schoolapp/model/ParentDetails.java
@@ -1,7 +1,10 @@
 package com.nickteck.schoolapp.model;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by admin on 6/18/2018.
@@ -9,6 +12,7 @@ import org.json.JSONObject;
 
 public class ParentDetails {
    private String Status_code,device_id,parent_id,parent_name;
+   ArrayList<student_details>student_details  = new ArrayList<>();
 
     public String getStatus_code() {
         return Status_code;
@@ -42,11 +46,66 @@ public class ParentDetails {
         this.parent_name = parent_name;
     }
 
+    public class student_details{
+        private String student_id,student_name,student_std,student_section,student_photo;
+
+        public String getStudent_id() {
+            return student_id;
+        }
+
+        public void setStudent_id(String student_id) {
+            this.student_id = student_id;
+        }
+
+        public String getStudent_name() {
+            return student_name;
+        }
+
+        public void setStudent_name(String student_name) {
+            this.student_name = student_name;
+        }
+
+        public String getStudent_std() {
+            return student_std;
+        }
+
+        public void setStudent_std(String student_std) {
+            this.student_std = student_std;
+        }
+
+        public String getStudent_section() {
+            return student_section;
+        }
+
+        public void setStudent_section(String student_section) {
+            this.student_section = student_section;
+        }
+
+        public String getStudent_photo() {
+            return student_photo;
+        }
+
+        public void setStudent_photo(String student_photo) {
+            this.student_photo = student_photo;
+        }
+    }
     public JSONObject toJSON(){
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("parent_id",parent_id);
             jsonObject.put("parent_name",parent_name);
+            JSONArray studentDetailArray = new JSONArray();
+            for (int i = 0 ; i < student_details.size(); i++){
+                ParentDetails.student_details studentDetails = student_details.get(i);
+                JSONObject studObject = new JSONObject();
+                studObject.put("student_id",studentDetails.getStudent_id());
+                studObject.put("student_name",studentDetails.getStudent_name());
+                studObject.put("student_std",studentDetails.getStudent_std());
+                studObject.put("student_section",studentDetails.getStudent_section());
+                studObject.put("student_photo",studentDetails.getStudent_photo());
+                studentDetailArray.put(studObject);
+            }
+            jsonObject.put("student_details",studentDetailArray);
         } catch (JSONException e) {
             e.printStackTrace();
         }
